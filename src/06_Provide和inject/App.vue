f5a0371e74af98e485a618e9617f4c3d9de57eb0
JLphile <lkyz001@outlook.com>
2022-09-12 22:04:38 +0800

    provide-inject

---
+++ src/06_Provide和inject/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <home></home>
+    <h2>App-counter:{{ counter }}</h2>
+    <button @click="increment">App+1</button>
+  </div>
+</template>
+
+<script>
+import { provide, ref, readonly } from 'vue';
+import Home from './Home.vue';
+export default {
+  components: { Home },
+  setup() {
+    const name = ref('coderwhy');
+    let counter = ref(100);
+
+    const increment = () => counter.value++;
+
+    provide('name', readonly(name));
+    provide('counter', readonly(counter));
+
+    return {
+      increment,
+      counter,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,6 @@
 import { createApp } from 'vue';
-import App from './04_computed和watch/App.vue';
+import App from './06_Provide和inject/App.vue';
 
 const app = createApp(App);
-// app.mixin({
-//   data() {
-//     return {};
-//   },
-//   methods: {},
-//   created() {
-//     console.log('全局的mixin中的created调用');
-//   },
-// });
+
 app.mount('#app');
